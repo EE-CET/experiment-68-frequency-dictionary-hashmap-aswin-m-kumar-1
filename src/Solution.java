690505eb52b6c9ff7d369c7edecdd16a2ff876f0
Aswin M Kumar <aswin2k03.mk@gmail.com>
2026-04-10 10:47:52 +0530

    Implement Word Frequency (HashMap)

--- src/Solution.java
+++ src/Solution.java
@@ -6,16 +6,21 @@ public class Solution {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         
-        // TODO: Read a single line of text
-        
-        // TODO: Split the text into individual words
-        
-        // TODO: Create a HashMap to store the frequency of each word
-        
-        // TODO: Iterate through the words and update their frequencies in the map
-        
-        // TODO: Iterate through the map and print the unique words and their counts
-        // Format: "word: count"
-        
+        if (scanner.hasNextLine()) {
+            String line = scanner.nextLine();
+            String[] words = line.split("\\s+");
+            HashMap<String, Integer> map = new HashMap<>();
+            
+            for (String word : words) {
+                if (!word.isEmpty()) {
+                    map.put(word, map.getOrDefault(word, 0) + 1);
+                }
+            }
+            
+            for (Map.Entry<String, Integer> entry : map.entrySet()) {
+                System.out.println(entry.getKey() + ": " + entry.getValue());
+            }
+        }
+        scanner.close();
     }
 }
