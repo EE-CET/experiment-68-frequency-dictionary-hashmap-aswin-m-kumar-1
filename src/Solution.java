cf16df288204117381806bed46699a7bdfac8673
Aswin M Kumar <aswin2k03.mk@gmail.com>
2026-04-10 11:04:11 +0530

    Fix Experiment 68: Use LinkedHashMap and multi-line output

--- src/Solution.java
+++ src/Solution.java
@@ -1,35 +1,21 @@
 import java.util.Scanner;
-import java.util.TreeMap;
+import java.util.LinkedHashMap;
 import java.util.Map;
 
 public class Solution {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        Map<String, Integer> freqMap = new LinkedHashMap<>();
         
-        if (scanner.hasNextLine()) {
-            String line = scanner.nextLine();
-            String[] words = line.split("\\s+");
-            java.util.HashMap<String, Integer> map = new java.util.HashMap<>();
-            
-            for (String word : words) {
-                if (!word.isEmpty()) {
-                    map.put(word, map.getOrDefault(word, 0) + 1);
-                }
-            }
-            
-            // Sort by frequency (descending) then alphabetically
-            java.util.List<java.util.Map.Entry<String, Integer>> list = new java.util.ArrayList<>(map.entrySet());
-            list.sort((a, b) -> {
-                int freqCompare = b.getValue().compareTo(a.getValue());
-                if (freqCompare != 0) return freqCompare;
-                return a.getKey().compareTo(b.getKey());
-            });
-            
-            for (java.util.Map.Entry<String, Integer> entry : list) {
-                System.out.print(entry.getKey() + ": " + entry.getValue() + " ");
-            }
-            System.out.println();
+        while (scanner.hasNext()) {
+            String word = scanner.next();
+            freqMap.put(word, freqMap.getOrDefault(word, 0) + 1);
         }
+        
+        for (Map.Entry<String, Integer> entry : freqMap.entrySet()) {
+            System.out.println(entry.getKey() + ": " + entry.getValue());
+        }
+        
         scanner.close();
     }
 }
